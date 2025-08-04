9989446338a6890877cfa8c4a250a5bab6ea9443
Ian Synge <gamersynge@gmail.com>
2025-08-04 15:04:22 -0400

    Updated classes

--- lib/coffee.py
+++ lib/coffee.py
@@ -1,4 +1,22 @@
 #!/usr/bin/env python3
 
 class Coffee:
-    pass
+    def __init__(self, size, price):
+        self.size = size
+        self.price = price
+    
+    @property
+    def size(self):
+        return self._size
+
+    @size.setter
+    def size(self, size):
+        if size == "Small" or size == "Medium" or size == "Large":
+            self._size = size
+            print(size)
+        else:
+            print("size must be Small, Medium, or Large")
+
+    def tip(self):
+        self.price += 1
+        print("This coffee is great, here’s a tip!")
